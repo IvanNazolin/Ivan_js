dab7e73cba0d164e6273fdc2417e3f9747c8e11d
IvanNazolin <125957878+IvanNazolin@users.noreply.github.com>
2023-05-21 15:26:11 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,111 +15,11 @@ softShadows()
 
 
 
-const HINT_PHRASE = "За колонку подойди и подарочек найди 🎁"
+const HINT_PHRASE = "Удачи на соревнованиях"
 //14 words
-const birthdayText = "Happy birthday to you Ivan I wish you good luck in new school 1502"
+const birthdayText = "seen ever have I what girl best the is you Nika"
+
 const word_list = birthdayText.split(" ").reverse()
-
-function Box({position, scale}) {
-  //const [ref, api] = useBox(() => ({ mass: 1, position: [0, 10, 0], onCollide: (e)=>e.body.material.color.g = Math.random() }))
-  const star = useRef()
-  // function handleClick() {
-  //   console.log(api)
-  //   api.velocity.set(0, 5, 0)
-  // }
-  const vector = [10*Math.random(), 10*Math.random(), 10*Math.random()]
-
-  useFrame(({clock}) => {
-    star.current.position.x += vector[0]/100
-    star.current.position.y += vector[1]/100
-    star.current.position.z += vector[2]/100
-  })
-
-  return (
-    <>
-
-      <mesh ref={star} position={position} receiveShadow castShadow scale={scale}>
-        <boxBufferGeometry attach="geometry" />
-        <meshStandardMaterial color="hotpink" />
-      </mesh>
-    </>
-  )
-}
-
-const CameraControls = () => {
-  // Get a reference to the Three.js Camera, and the canvas html element.
-  // We need these to setup the OrbitControls class.
-  // https://threejs.org/docs/#examples/en/controls/OrbitControls
-
-  const {
-    camera,
-    gl: { domElement }
-  } = useThree();
-
-  // Ref to the controls, so that we can update them on every frame using useFrame
-  const controls = useRef();
-  useFrame(state => controls.current.update());
-  return (
-    <orbitControls
-      ref={controls}
-      args={[camera, domElement]}
-      enableZoom={true}
-    />
-  );
-};
-
-function BillBoard({ position }) {
-  return (
-    <Billboard
-      follow={true}
-      lockX={false}
-      lockY={false}
-      lockZ={false} // Lock the rotation on the z axis (default=false)
-      position={position}
-      children={
-        <LoveText rotation={[0, 0, 0]} fontSize={5} maxWidth={20} lineHeight={1} textAlign="center">
-          My Baby!
-          My princess!
-          I love you so much ❤
-        </LoveText>
-      }
-    />
-  )
-}
-
-function FallingStars({ count }) {
-  const positions = useMemo(() => {
-    let positions = []
-    for (let i = 0; i < count; i++) {
-      const r = 3600
-      const theta = 2 * Math.PI * Math.random()
-      const phi = Math.acos(2 * Math.random() - 1)
-      const x = r * Math.cos(theta) * Math.sin(phi) + (-2000 + Math.random() * 4000)
-      const y = r * Math.sin(theta) * Math.sin(phi) + (-2000 + Math.random() * 4000)
-      const z = r * Math.cos(phi) + (-1000 + Math.random() * 2000)
-      positions.push(x)
-      positions.push(y)
-      positions.push(z)
-    }
-    return new Float32Array(positions)
-  }, [count])
-
-  let group = useRef();
-
-  useFrame(() => (group.current.rotation.y -= 0.001))
-
-  return (
-    <group ref={group}>
-      <points>
-        <bufferGeometry attach="geometry">
-          <bufferAttribute attachObject={['attributes', 'position']} count={positions.length / 3} array={positions} itemSize={3} />
-        </bufferGeometry>
-        <pointsMaterial attach="material" size={12.5} sizeAttenuation color="red" fog={false} />
-      </points>
-    </group>
-  )
-}
-
 
 function Heart(props) {
   const { gl, camera } = useThree()
@@ -177,30 +77,8 @@ function Heart(props) {
           metalness={0.3}
         />
       </mesh>
-      <LoveText rotation={[-Math.PI/2, group.current ? group.current.rotation.z : 0, -Math.PI]} position={[0, 0, 25]} fontSize={7}>Happy Birthday, Ivan!</LoveText>
+      <LoveText rotation={[-Math.PI/2, group.current ? group.current.rotation.z : 0, -Math.PI]} position={[0, 0, 25]} fontSize={7}>Good luck, Nika!</LoveText>
     </group>
-  )
-}
-
-
-function MyPlane({ position, width, height, color }) {
-  const [ref] = usePlane(() => ({ position: [0, 0, 0], rotation: [-Math.PI / 2, 0, 0] }))
-
-  return (
-    <>
-    <mesh position={[position[0], position[1], position[2]]} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[width, height]} />
-      <shadowMaterial attach="material" opacity={0.1}/>
-    </mesh>
-    <mesh position={[position[0], position[1], position[2]]} rotation={[-Math.PI / 2, 0, 0]} ref={ref} receiveShadow={true}>
-      <planeBufferGeometry attach="geometry" args={[width, height]} />
-      <meshStandardMaterial
-        attach="material"
-        side={THREE.DoubleSide}
-        color="lightblue"
-      />
-    </mesh>
-    </>
   )
 }
 
@@ -232,16 +110,6 @@ function LoveText({ rotation, children, fontSize, maxWidth, lineHeight, textAlig
         color="red"
       />
     </Text>
-  )
-}
-
-function Ball(){
-  const [ref, api] = useSphere(() => ({mass: 1, position: [1, 4, 1]}))
-  return(
-    <mesh ref={ref} castShadow position = {[1, 4, 1]}>
-      <SphereBufferGeometry attach="geometry" />
-      <MeshStandardMaterial attach="material" color="red"/>
-    </mesh>
   )
 }
 
@@ -304,54 +172,6 @@ function Cloud({ count = 10, radius = 20 }) {
   return words.map(([pos, word], index) => <Word key={index} position={pos} children={word} index={index}/>)
 }
 
-const AnimatedCamera = ({ animate }) => {
-  const [dollyFinished, setDollyFinished] = useState(false);
-  const controls = useRef();
-
-  const { y, x } = useSpring({
-    from: { y: 20, x: -20 },
-    to: animate ? { y: 0, x: -2 } : {},
-    config: {
-      mass: 1.5,
-      tension: 280,
-      friction: 140
-    },
-    onRest: () => setDollyFinished(true)
-  });
-
-  const { targetY } = useSpring({
-    from: { targetY: 20 },
-    to: animate ? { targetY: 0 } : {},
-    config: {
-      tension: 330,
-      friction: 70
-    }
-  });
-
-  useFrame(({ camera }) => {
-    if (!dollyFinished) {
-      controls.current.target.y = targetY.value;
-
-      if (camera.position.y > 0) {
-        camera.position.y = y.value;
-        camera.position.x = x.value;
-        controls.current.update();
-      }
-    }
-  });
-
-  return (
-    <>
-      <OrbitControls
-        enabled={true}
-        ref={controls}
-        target={[0, 20, 0]}
-      />
-      <PerspectiveCamera makeDefault position={[-20, 20, 4]} />;
-    </>
-  );
-};
-
 function App() {
   const canvasRef = useRef()
   const radius = 3600
@@ -375,16 +195,9 @@ function App() {
       
       
 
-      <OrbitControls enabled={animationFinished} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2}/>
+      <OrbitControls enabled={animationFinished} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2} enablePan={false}/>
 
       <Suspense fallback={null}><Heart setAnimationFinished={setAnimationFinished} animationFinished={animationFinished}/></Suspense>
-      
-      {/* <Physics>
-        
-        <MyPlane position={[0, -1, 0]} width={10} height={10} color="lightblue" />
-        
-      </Physics> */}
-
       
       <Cloud count={14} radius={40} /> 
 
